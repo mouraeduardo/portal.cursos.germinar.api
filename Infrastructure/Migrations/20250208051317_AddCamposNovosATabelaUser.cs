54281be916acf5145bf4d224c242298159f2f270
Eduardo Moura <eduardo.moura@manyminds.com>
2025-02-08 02:50:00 -0300

    Migrations: Add novos campos a tabela 'User"

---
+++ Infrastructure/Migrations/20250208051317_AddCamposNovosATabelaUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCamposNovosATabelaUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Registration",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Registration",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "users");
+        }
+    }
+}
